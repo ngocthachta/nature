02a1ca1c3359a30fb38b4b461b8ddee6ae8d579d
HOME Nature <ngocthachbk@gmail.com>
2016-01-17 23:39:30 +0100

    update pdfs join with quality customize

--- scripts/pdfs_join.sh
+++ scripts/pdfs_join.sh
@@ -1,2 +1,4 @@
 # this script joins all the pdfs inputs files to file results.pdf
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=results.pdf $@
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dDownsampleColorImages=true \
+-dColorImageResolution=100 -dPDFSETTING=/ebook -dNOPAUSE -dQUIET -dBATCH \
+-sOutputFile=results.pdf $@
